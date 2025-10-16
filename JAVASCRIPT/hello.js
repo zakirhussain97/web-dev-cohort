ca2b476eaf785f0939468d8ec128aa627789a8b6
Zakir Hussain Parrey <zakirhussainparrey@gmail.com>
2025-10-16 09:29:00 +0530

    Add files via upload

---
+++ JAVASCRIPT/hello.js
@@ -0,0 +1,16 @@
+function printChai() {
+  console.log("Hello chai");
+}
+
+function bringBrush(numberOfBrush) {
+  console.log(`Hanji, le aaya ${numberOfBrush} brush`);
+}
+
+function addTwo(ekNum, doNum) {
+  return ekNum + doNum;
+}
+
+bringBrush(14);
+printChai();
+addTwo(1, 1);
+console.log(addTwo(2, 2));
